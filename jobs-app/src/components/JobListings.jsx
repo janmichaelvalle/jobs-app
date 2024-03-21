b95c67a41ad7912237902049312a05acdeef79a7
janmichaelvalle <janmichaelvalle@gmail.com>
2024-03-21 21:01:59 +0800

    Add json server and refactor joblistings page

--- jobs-app/src/components/JobListings.jsx
+++ jobs-app/src/components/JobListings.jsx
@@ -1,9 +1,50 @@
 import React from "react";
 import jobs from "../jobs.json";
 import JobListing from "./JobListing";
+import { useState, useEffect } from "react";
 
 const JobListings = ( {isHome = false } ) => {
-  const jobListings = isHome ? jobs.slice(0, 3) : jobs
+  // const jobListings = isHome ? jobs.slice(0, 3) : jobs
+
+  // intial set of jobs is an empty array
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    /* 
+    
+    const fetchJobs = () => {
+      fetch("http://localhost:8000/jobs")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setJobs(data)
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+      
+    }
+    */
+
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch("http://localhost:8000/jobs")
+        const data = await res.json();
+        setJobs(data)
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchJobs()
+  }, [])
+  
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -11,7 +52,7 @@ const JobListings = ( {isHome = false } ) => {
           { isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobListings.map((job) => (
+          {jobs.map((job) => (
             <JobListing key ={job.id} job={job}/>
           ))}
         </div>
