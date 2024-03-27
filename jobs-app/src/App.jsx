eec08b5be0fdc0699da786ec12309acc588fabb6
Mikee <100498679+janmichaelvalle@users.noreply.github.com>
2024-03-27 11:05:55 +0800

    Finish adding jobs

--- jobs-app/src/App.jsx
+++ jobs-app/src/App.jsx
@@ -9,11 +9,25 @@ import AddJobPage from "./pages/AddJobPage";
 
 
 const App = () => {
-  
-  const addJob = (newJob) => {
 
-    console.log(newJob)
-  
+  // Add new job
+  const addJob = async (newJob) => {
+
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+
+  // Delete job
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE',
+    })
   }
   
   const router = createBrowserRouter(
@@ -23,7 +37,7 @@ const App = () => {
         <Route index element={<HomePage/>}/>
         <Route path="/jobs" element={<JobsPage/>}/>
         <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob}/>}/>
-        <Route path="/jobs/:id" element={<JobPage/>} loader={jobLoader}/>
+        <Route path="/jobs/:id" element={<JobPage deleteJob={ deleteJob }/>} loader={jobLoader}/>
         <Route path="*" element={<NotFoundPage/>}/>
       </Route>
   )
--- jobs-app/src/pages/JobPage.jsx
+++ jobs-app/src/pages/JobPage.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 // import {useState, useEffect } from 'react';
 // import Spinner from '../components/Spinner';
-import { useParams, useLoaderData, Link } from "react-router-dom";
+import { useParams, useLoaderData, Link, useNavigate } from "react-router-dom";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
+import {toast} from 'react-toastify';
 
 
-const JobPage = () => {
+const JobPage = ( {deleteJob}) => {
+  const navigate = useNavigate()
   const { id } = useParams(); // Destructure useParams to get id
   const job = useLoaderData();
+
+  const onDeleteClick = (jobId) => {
+    const confirm = window.confirm("Are you sure you want to delete this listing?")
+    if(!confirm) return;
+
+    deleteJob(jobId);
+
+    toast.success('Job deleted successfully');
+
+    navigate("/jobs")
+  }
 
   /* 
   const [job, setJob] = useState(null);
@@ -38,7 +51,7 @@ const JobPage = () => {
             to="/jobs"
             className="text-indigo-500 hover:text-indigo-600 flex items-center"
           >
-            <FaArrowLeft class="mr-2"/>
+            <FaArrowLeft className="mr-2"/>
             Back to Job Listings
           </Link>
         </div>
@@ -110,7 +123,7 @@ const JobPage = () => {
                 >
                   Edit Job
                 </Link>
-                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                <button onClick= { () => onDeleteClick(job.id)}className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
                   Delete Job
                 </button>
               </div>
